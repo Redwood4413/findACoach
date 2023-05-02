cef9b227c2ad03a4885223f1a2f9e3847918f8bf
Redwood <redwoodredwoodowski@gmail.com>
2023-05-02 18:11:02 +0200

    chore: development

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,20 +1,66 @@
 <script lang="ts">
+import BaseButton from '../UI/BaseButton.vue';
+import TheHeaderSidebar from './TheHeaderSidebar.vue';
+import HamburgerIcon from '../icons/HamburgerIcon.vue'
+
 export default {
-  name: 'TheHeader',
+    name: "TheHeader",
+    data: () => ({
+        small: false as boolean,
+        isVisible: true as boolean,
+    }),
+    methods: {
+        handleScroll(event: any) {
+            if (event.currentTarget.scrollY > 50) {
+                if (!this.small) {
+                    this.small = true;
+                }
+            }
+            else {
+                if (this.small) {
+                    this.small = false;
+                }
+            }
+        },
+        handleResize(event: any) {
+          if (window.innerWidth <= 500) {
+            this.isVisible = false;
+          } else {
+            this.isVisible = true;
+          }
+        }
+    },
+    computed: {
+        isSmall(): string {
+            return this.small ? "small" : "";
+        }
+    },
+    mounted() {
+        window.addEventListener("scroll", this.handleScroll);
+        window.addEventListener("resize", this.handleResize);
+    },
+    unmounted() {
+        window.removeEventListener("scroll", this.handleScroll);
+    },
+    components: { BaseButton, TheHeaderSidebar, HamburgerIcon }
 };
 </script>
 
 <template>
-  <header class="header">
+  <header :class="`header ${isSmall}`">
     <RouterLink :to="{ name: 'home' }">
       <h2>Find a Coach</h2>
     </RouterLink>
-    <div class="buttons">
-      <RouterLink :to="{ name: 'register' }">Register</RouterLink>
+    <div class="buttons" v-if="isVisible">
+      <RouterLink :to="{ name: 'coaches' }">Coaches</RouterLink>
       <RouterLink :to="{ name: 'requests' }">Requests</RouterLink>
-
+      <RouterLink :to="{ name: 'register' }">Register</RouterLink>
     </div>
+    <BaseButton mode="flat" v-else>
+      <HamburgerIcon></HamburgerIcon>
+    </BaseButton>
   </header>
+  <TheSidebar v-if="!isVisible"></TheSidebar>
 </template>
 
 <style lang="scss" scoped>
@@ -28,18 +74,32 @@ export default {
     border-bottom: 2px solid white;
     background: rgb(255,255,255, 0.2);
     backdrop-filter: blur(3px);
-  }
-  .buttons {
-    display:flex;
-    height:100%;
-    a {
-      background: rgb(0,0,0, 0.2);
-      padding:1rem;
-      transition: background .1s ease-in-out;
-      &:hover {
-      background: rgb(0,0,0, 0.3);
-
+    transition: all .3s ease-in-out;
+    &.small {
+      font-size: small;
+      padding:0 1rem;
+    }
+      .buttons {
+      display:flex;
+      height:100%;
+      a {
+        padding:0.8rem;
+        transition: background .1s ease-in-out;
+        &:hover:not(.router-link-exact-active) {
+          background: rgb(0,0,0, 0.3);
+        }
       }
+    }
+  }
+
+  .router-link-exact-active {
+    background: rgb(32, 114, 54);
+    border-bottom: 2px solid rgb(51, 161, 80);
+  }
+
+  @media (width < 500px) {
+    .buttons {
+      
     }
   }
 </style>
---
+++ src/components/layouts/TheHeaderSidebar.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+export default {
+  name: 'TheHeaderSidebar',
+  props: ['expanded']
+};
+</script>
+
+<template>
+  <div class="side-bar">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .side-bar {
+    position: fixed;
+    right:0;
+    height:100%;
+    width: 400px;
+    background: #000;
+  }
+  @media (width <= 500px) {
+    .side-bar {
+      width:80vw;
+    }
+  }
+</style>
