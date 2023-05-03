2743fd98669d86c63a83ed37a6e7df1e4bcedbdf
Redwood <redwoodredwoodowski@gmail.com>
2023-05-03 23:40:26 +0200

    feat: vuex added

---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex';
+import coaches from './modules/coaches/index';
+import requests from './modules/requests/index';
+
+const store = createStore({
+  modules: {
+    coaches,
+    requests,
+  },
+});
+
+export default store;
