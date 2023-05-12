084f7597bf554a62cb9759ca101fcfda6f837d24
Redwood <redwoodredwoodowski@gmail.com>
2023-05-13 01:13:50 +0200

    feat: add type to vuex

--- src/vuex.d.ts
+++ src/vuex.d.ts
@@ -3,9 +3,21 @@ import { Store } from 'vuex';
 declare module '@vue/runtime-core' {
   // declare your own store states
   interface State {
-    count: number
+    coaches: CoachesState[],
+    filterArray: string[],
+    areas: Areas,
   }
-
+  interface CoachesState {
+    id: string,
+    firstName: string,
+    lastName: string,
+    areas: string[]
+    description: string,
+    hourlyRate: number,
+  }
+  interface Areas {
+    areas: string[];
+  }
   // provide typings for `this.$store`
   interface ComponentCustomProperties {
     $store: Store<State>
