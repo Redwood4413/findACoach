724998c57bf0a962e64b5c8b6fd4c18c9ec73e42
Redwood <redwoodredwoodowski@gmail.com>
2023-05-13 01:13:59 +0200

    feat: init

---
+++ src/components/coaches/BadgeItem.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+
+export default {
+  name: 'BadgeItem',
+  props: {
+    area: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <li class="badge">
+    {{ area }}
+  </li>
+</template>
+
+<style lang="scss" scoped>
+@use '@/colors.scss';
+.badge {
+  display:flex;
+  background: colors.$strong-red;
+  padding:0 0.5rem;
+  border-radius: 5rem;
+  text-transform: capitalize;
+  font-style: oblique;
+  font-size: smaller;
+}
+</style>
