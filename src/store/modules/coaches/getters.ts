94c430eb1493aff841d6ae57a0b7b74779df573d
Redwood <redwoodredwoodowski@gmail.com>
2023-05-13 01:19:56 +0200

    feat: add getters with filter options

--- src/store/modules/coaches/getters.ts
+++ src/store/modules/coaches/getters.ts
@@ -1,3 +1,15 @@
-export default {
+import { State } from 'vue';
 
+export default {
+  coaches(state: State) {
+    const filter = state.filterArray;
+
+    // eslint-disable-next-line vue/max-len
+    return state.coaches.filter((coach) => coach.areas.some((area: string) => filter.includes(area)));
+  },
+  areas(state: State) {
+    const allAreas = state.coaches.flatMap((coach) => coach.areas);
+    const uniqueAreas = Array.from(new Set(allAreas));
+    return uniqueAreas;
+  },
 };
