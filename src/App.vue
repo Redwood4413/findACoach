7a232594959c0eef19c8ab6f85695e85ddd5b58d
Redwood <redwoodredwoodowski@gmail.com>
2023-05-18 20:57:18 +0200

    fix: move several styles from style.scss

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,42 @@ export default {
 </template>
 
 <style lang="scss">
-@use './colors.scss';
+@use '@/colors.scss';
+@use '@/variables.scss';
 
 :root {
   background-color: colors.$background-0;
   color: colors.$foreground-0;
 }
 
+body {
+  margin: 0;
+  min-width: 320px;
+  min-height: 100vh;
+  &.hiden-overflow {
+    overflow: hidden;
+    margin: 0 variables.$scrollbar-width 0 0;
+  }
+}
+
 * {
   box-sizing: border-box;
+  scrollbar-width: auto;
+  scrollbar-color: red #ffffff;
+}
+
+*::-webkit-scrollbar {
+  width: variables.$scrollbar-width;
+}
+
+*::-webkit-scrollbar-track {
+  background: colors.$background-soft;
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: colors.$yellow;
+  border-radius: 20px;
+  border: 0px solid #ffffff;
 }
 
 a {
@@ -31,11 +58,16 @@ a {
   text-decoration: inherit;
 }
 
+img, svg, video {
+  max-width: 100%;
+  display:block;
+}
 #app {
   width: 100%;
   margin: 0 auto;
   min-height: 2000px;
   text-align: center;
+
 }
 
 .underline {
