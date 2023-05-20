6614b8430cb277ef9809f1f3100b7129b6d68eec
Redwood <redwoodredwoodowski@gmail.com>
2023-05-21 00:02:55 +0200

    fix: set to proper component name

---
+++ src/components/coaches/CoachContact.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'CoachContact',
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    ...mapGetters('coaches', ['getCoach']),
+  },
+  mounted() {
+
+  },
+};
+</script>
+
+<template>
+  <BaseDialog returnTo="contact">
+    <h2 />
+  </BaseDialog>
+</template>
