1dac8e2c85d10f2dc050003c1fbdac22a00ac656
Redwood <redwoodredwoodowski@gmail.com>
2023-05-21 00:01:02 +0200

    feat: rename from BadgeItem

---
+++ src/components/coaches/CoachAreasList.vue
@@ -0,0 +1,43 @@
+<script lang="ts">
+import CoachAreasItem from './CoachAreasItem.vue';
+
+export default {
+  name: 'CoachAreasList',
+  props: {
+    areas: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: { CoachAreasItem },
+};
+</script>
+
+<template>
+  <ul class="areas-list">
+    <span class="section-title">areas:</span>
+    <div class="list-wrapper">
+      <CoachAreasItem
+        v-for="(area, index) in areas"
+        :key="index"
+        :area="area"
+      />
+    </div>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+@use '@/colors.scss';
+.areas-list {
+    display:flex;
+    flex-direction: column;
+    min-width:200px;
+    padding:0;
+    .list-wrapper {
+      display:flex;
+      place-items: center;
+      flex-wrap: wrap;
+      gap:5px;
+    }
+  }
+</style>
