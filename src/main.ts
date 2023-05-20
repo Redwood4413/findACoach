adb39a692309af7a35c798aaeb2d073471795b27
Redwood <redwoodredwoodowski@gmail.com>
2023-05-20 23:57:07 +0200

    fix: make base components available globally

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import './style.scss';
 import App from './App.vue';
 import router from './router';
 import store from './store/index';
+import BaseButton from './components/UI/BaseButton.vue';
+import BaseDialog from './components/UI/BaseDialog.vue';
+import BaseWrapper from './components/UI/BaseWrapper.vue';
+import BaseRouterLink from './components/UI/BaseRouterLink.vue';
 
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
+app.component('BaseButton', BaseButton);
+app.component('BaseDialog', BaseDialog);
+app.component('BaseWrapper', BaseWrapper);
+app.component('BaseRouterLink', BaseRouterLink);
+
 app.mount('#app');
