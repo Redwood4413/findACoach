2010cfd302f87f49c601540981b8d47147011517
Redwood <redwoodredwoodowski@gmail.com>
2023-05-20 23:59:34 +0200

    feat: improve component

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,11 +1,30 @@
 <script lang="ts">
+
 export default {
   name: 'NotFound',
+  props: {
+    element: {
+      type: String,
+      required: true,
+    },
+  },
 };
 </script>
 
 <template>
-  <div>
-    Nothing found
+  <div class="nothing-found">
+    <h2>{{ element }} not found.</h2>
+    <BaseRouterLink mode="rounded" :to="{ name: 'home' }">Return</BaseRouterLink>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .nothing-found {
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height:100%;
+    width:100%;
+  }
+</style>
