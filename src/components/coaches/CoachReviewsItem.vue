03af1d01a809d53f415b7f04619a42f203e65939
Redwood <redwoodredwoodowski@gmail.com>
2023-07-16 14:30:45 +0200

    feat: init

---
+++ src/components/coaches/CoachReviewsItem.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { PropType } from 'vue';
+import { Review } from '@/stores/CoachesStore';
+
+export default {
+  name: 'CoachReviewsItem',
+  props: {
+    review: {
+      type: Object as PropType<Review>,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <li>{{ review.rate }}</li>
+</template>
+
+<style lang="scss" scoped>
+@use '@/colors.scss';
+
+</style>
---
+++ src/components/coaches/CoachReviewsList.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+
+import { useCoachesStore } from '@/stores/CoachesStore';
+import CoachReviewsItem from './CoachReviewsItem.vue';
+
+export default {
+  name: 'CoachReviewsList',
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    const coachStore = useCoachesStore();
+    return { coachStore };
+  },
+  computed: {
+    reviews() {
+      return this.coachStore.getReviews(this.id);
+    },
+  },
+  components: { CoachReviewsItem },
+};
+</script>
+
+<template>
+  <ul class="reviews-list">
+    <CoachReviewsItem
+      v-for="(review, index) in reviews"
+      :key="index"
+      :review="review" />
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+@use '@/colors.scss';
+
+</style>
