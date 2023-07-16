ca959da0df3383fa6b86d910e89e761192f57271
Redwood <redwoodredwoodowski@gmail.com>
2023-07-16 14:31:53 +0200

    feat: delete stroke-width attribute

--- src/components/icons/CloseIcon.vue
+++ src/components/icons/CloseIcon.vue
@@ -8,7 +8,7 @@ export default {
 
   <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g id="Menu / Close_SM">
-      <path id="Vector" d="M16 16L12 12M12 12L8 8M12 12L16 8M12 12L8 16" stroke="#FBF1C7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      <path id="Vector" d="M16 16L12 12M12 12L8 8M12 12L16 8M12 12L8 16" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
     </g>
   </svg>
 </template>
