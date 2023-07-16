f8d61cb4d6485a138a848647c0f107a37390e9e7
Redwood <redwoodredwoodowski@gmail.com>
2023-07-16 14:31:01 +0200

    feat: init

---
+++ src/components/icons/LeftArrow.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+export default {
+  name: 'LeftArrow',
+};
+</script>
+
+<template>
+  <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M15 20L7 12L15 4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+  </svg>
+</template>
