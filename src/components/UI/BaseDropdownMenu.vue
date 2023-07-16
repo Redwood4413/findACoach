c2de1a992f44f39619a20e0f2c8bd899b81f6d46
Redwood <redwoodredwoodowski@gmail.com>
2023-07-16 14:33:59 +0200

    feat: init

---
+++ src/components/UI/BaseDropdownMenu.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+
+export default {
+  name: 'BaseDropdownMenu',
+};
+</script>
+
+<template>
+  <div class="dropdown-menu">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/colors.scss';
+  .dropdown-menu {
+    position: absolute;
+    top:100%;
+    left:50%;
+    width:100%;
+    overflow: hidden;
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    transform: translate(-50%, 5%);
+    border:2px solid colors.$gray;
+    border-radius: 10px;
+    background: colors.$background-1;
+  }
+
+</style>
