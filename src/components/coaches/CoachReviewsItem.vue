be8a6a54174fb944e3b214a2693ed232988e17ba
Redwood <redwoodredwoodowski@gmail.com>
2023-08-04 01:11:26 +0200

    feat: introduce reviews list

--- src/components/coaches/CoachReviewsItem.vue
+++ src/components/coaches/CoachReviewsItem.vue
@@ -1,6 +1,9 @@
 <script lang="ts">
 import { PropType } from 'vue';
-import { Review } from '@/stores/CoachesStore';
+import { useUsersStore } from '@/stores/UsersStore';
+import CoachRatePoint from './CoachRatePoint.vue';
+
+import UserAvatar from '../users/UserAvatar.vue';
 
 export default {
   name: 'CoachReviewsItem',
@@ -9,15 +12,140 @@ export default {
       type: Object as PropType<Review>,
       required: true,
     },
+    quantity: {
+      type: Number,
+      required: true,
+    },
   },
+  setup() {
+    const usersStore = useUsersStore();
+    return { usersStore };
+  },
+  computed: {
+    reviewRate() {
+      return this.review.rate;
+    },
+    fullName(): string {
+      return this.usersStore.getFullName(this.review.authorId);
+    },
+    timeAdded(): string {
+      const options: Intl.DateTimeFormatOptions = {
+        dateStyle: 'short',
+        timeStyle: 'short',
+      };
+
+      const formatter = new Intl.DateTimeFormat(undefined, options);
+      return formatter.format(this.review.addedAt);
+    },
+  },
+  components: { CoachRatePoint, UserAvatar },
 };
 </script>
 
 <template>
-  <li>{{ review.rate }}</li>
+  <li class="coach-review">
+    <div class="header">
+      <div class="user-wrapper">
+        <UserAvatar class="small" />
+        <div class="user-info">
+          <span class="user-name">
+            {{ fullName }}
+          </span>
+          <div class="quantity">Reviews: ({{ quantity }})</div>
+        </div>
+      </div>
+      <div class="rate">
+        <div class="section-title header">
+          <span>Rate:</span>
+          <span> {{ reviewRate }}/5</span>
+        </div>
+        <div class="rate-wrapper">
+          <CoachRatePoint
+            v-for="(index) in 5"
+            :key="index"
+            :rate="reviewRate"
+            :index="index"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="description">
+      <div class="section-title">description:</div>
+      {{ review.description }}
+    </div>
+    <div class="time-added">
+      <div class="section-title">Added:</div>
+      <span>{{ timeAdded }}</span>
+    </div>
+  </li>
+
 </template>
 
 <style lang="scss" scoped>
 @use '@/colors.scss';
 
+  .coach-review {
+    display:flex;
+    flex:1;
+    flex-direction: column;
+    padding:1em 0;
+    border-bottom: colors.$strong-gray 1px solid;
+    .header{
+      display:flex;
+      .user-wrapper {
+        display:flex;
+        gap:1em;
+        .user-info {
+          display:flex;
+          justify-content: center;
+          flex-direction: column;
+          .user-name {
+            font-weight: 400;
+          }
+          .quantity {
+            font-size: x-small;
+            color: colors.$foreground-3;
+          }
+        }
+      }
+      .rate {
+        display:flex;
+        text-align: end;
+        flex-direction: column;
+        margin-left:auto;
+        .header {
+          display:flex;
+          justify-content: space-between;
+        }
+        .rate-wrapper {
+          display:flex;
+          gap:0.5em;
+        }
+      }
+    }
+
+    .description {
+      display:flex;
+      flex-direction: column;
+      font-weight: 300;
+      font-size: 0.9em;
+    }
+    .time-added {
+      color: colors.$foreground-2;
+      font-size: smaller;
+      margin-left: auto;
+      font-style: italic;
+      text-align: right;
+      font-family: 'Open Sans';
+    }
+  }
+    // .controls {
+    //   display:flex;
+    //   flex-direction: column;
+    //   .controls-wrapper {
+    //     display:flex;
+    //     flex-wrap: wrap;
+    //     gap: 5px;
+    //   }
+    // }
 </style>
--- src/components/coaches/CoachReviewsList.vue
+++ src/components/coaches/CoachReviewsList.vue
@@ -1,39 +1,58 @@
 <script lang="ts">
 
-import { useCoachesStore } from '@/stores/CoachesStore';
+import { useReviewsStore } from '@/stores/ReviewsStore';
 import CoachReviewsItem from './CoachReviewsItem.vue';
+
+import NotFound from '../NotFound.vue';
 
 export default {
   name: 'CoachReviewsList',
   props: {
-    id: {
+    coachId: {
       type: String,
       required: true,
     },
   },
   setup() {
-    const coachStore = useCoachesStore();
-    return { coachStore };
+    const reviewsStore = useReviewsStore();
+    return { reviewsStore };
   },
   computed: {
     reviews() {
-      return this.coachStore.getReviews(this.id);
+      return this.reviewsStore.getReviews(this.coachId);
+    },
+    reviewsQuantity() {
+      return this.reviews.length;
     },
   },
-  components: { CoachReviewsItem },
+  methods: {
+    userReviewsQuantity(authorId: string) {
+      return this.reviewsStore.getAuthorReviews(authorId).length;
+    },
+  },
+  components: { CoachReviewsItem, NotFound },
 };
 </script>
 
 <template>
-  <ul class="reviews-list">
+  <ul class="reviews-list" v-if="reviews.length > 0">
     <CoachReviewsItem
       v-for="(review, index) in reviews"
       :key="index"
-      :review="review" />
+      :review="review"
+      :quantity="userReviewsQuantity(review.authorId)"
+    />
   </ul>
+  <NotFound element="Reviews" v-else />
 </template>
 
 <style lang="scss" scoped>
 @use '@/colors.scss';
 
+.reviews-list {
+  display:flex;
+  padding:0;
+  gap:1.5em;
+  flex-direction: column;
+}
 </style>
