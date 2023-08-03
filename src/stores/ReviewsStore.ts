33d4afc756b121b3213642465c61ad5fde9fb199
Redwood <redwoodredwoodowski@gmail.com>
2023-08-04 01:18:36 +0200

    feat: connect backend to the store

--- src/stores/ReviewsStore.ts
+++ src/stores/ReviewsStore.ts
@@ -1,38 +1,63 @@
 import { defineStore } from 'pinia';
 
-export interface Review {
-  coachId: string,
-  authorId: string,
-  description: string,
-  rate: number,
-}
-type Id = string | null;
 // eslint-disable-next-line import/prefer-default-export
 export const useReviewsStore = defineStore('reviewsStore', {
   state: () => ({
-    reviews: [{
-      coachId: 'c1',
-      authorId: 'u1',
-      description: 'Coach with a great knowledge.',
-      rate: 5,
-    },
-    {
-      coachId: 'c1',
-      authorId: 'u2',
-      description: 'Coach with a great knowledge.',
-      rate: 4,
-    }] as Review[],
+    reviews: [] as Review[],
   }),
   getters: {
     getReviews() {
-      return (coachId?: Id, authorId?: Id): Review[] => {
-        if (!coachId && !authorId) {
-          throw new Error('At least 1 argument is needed!');
-        }
-        const reviews = this.reviews.filter((review) => (coachId ? review.coachId === coachId
-          : review.authorId === authorId));
+      return (coachId: string): Review[] => {
+        const reviews = this.reviews.filter((review) => review.coachId === coachId);
+
         return reviews;
       };
+    },
+    getAuthorReviews() {
+      return (authorId: string): Review[] => {
+        const reviews = this.reviews.filter((review) => review.authorId === authorId);
+
+        return reviews;
+      };
+    },
+    getRate() {
+      return (coachId: string): number => {
+        let total = 0;
+        const reviews = this.getReviews(coachId);
+
+        if (!reviews.length) return 0;
+        reviews.forEach((review) => {
+          total += review.rate;
+        });
+        const average = total / reviews.length;
+        return Math.round(average);
+      };
+    },
+    getReviewsQuantity() {
+      return (coachId: string): number => this.getReviews(coachId).length;
+    },
+  },
+  actions: {
+    async fetchReviews(coachId: string) {
+      const url = `${import.meta.env.VITE_FIREBASE_URL}/reviews/${coachId}.json`;
+      try {
+        const response = await fetch(url);
+        const parsedResponse = await response.json();
+        console.log(parsedResponse);
+
+        if (!response.ok) {
+          throw new Error('Error');
+        }
+
+        Object.keys(parsedResponse).forEach((key) => {
+          this.setReview(parsedResponse[key]);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    setReview(review: Review) {
+      this.reviews.push(review);
     },
   },
 });
