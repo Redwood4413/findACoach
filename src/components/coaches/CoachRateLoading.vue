6bc97df6a9cfeb6e002b16b7afc231ae6a7a6267
Redwood <redwoodredwoodowski@gmail.com>
2023-08-04 01:08:30 +0200

    feat: init

---
+++ src/components/coaches/CoachRateLoading.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import SvgSpinnersRingResize from '../icons/animated/SvgSpinnersRingResize.vue';
+
+export default {
+  name: 'CoachRateLoading',
+  components: { SvgSpinnersRingResize },
+};
+</script>
+
+<template>
+  <div class="info">
+    <SvgSpinnersRingResize />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .info {
+    padding:5em;
+    display:flex;
+    flex-direction: column;
+    place-items: center;
+  }
+</style>
