1c951fbb3fb0ef38afa5c0c03e9b189e9d64b2eb
Redwood <redwoodredwoodowski@gmail.com>
2023-08-04 01:10:40 +0200

    feat: rebuild components

--- src/components/coaches/CoachReviews.vue
+++ src/components/coaches/CoachReviews.vue
@@ -1,11 +1,12 @@
 <script lang="ts">
 import { useCoachesStore } from '@/stores/CoachesStore';
 import NotFound from '../NotFound.vue';
-import CoachAvatar from './CoachAvatar.vue';
 import CoachReviewsList from './CoachReviewsList.vue';
 
+import CoachWrapperHeader from './CoachWrapperHeader.vue';
+
 export default {
-  name: 'CoachContact',
+  name: 'CoachReviews',
   props: {
     id: {
       type: String,
@@ -24,43 +25,27 @@ export default {
     isFound(): boolean {
       return !!this.coach;
     },
-    fullName(): string {
-      return `${this.coach?.firstName} ${this.coach?.lastName}`;
-    },
   },
   components: {
     NotFound,
-    CoachAvatar,
     CoachReviewsList,
+    CoachWrapperHeader,
   },
 };
 </script>
 
 <template>
   <div class="coach-reviews" v-if="isFound">
-    <div class="coach">
-      <CoachAvatar class="small" />
-      <span class="name">{{ fullName }}</span>
-    </div>
-    <CoachReviewsList :id="id" />
+    <CoachWrapperHeader :id="id" />
+    <CoachReviewsList :coachId="id" />
   </div>
   <NotFound element="Coach" v-else />
 </template>
 
 <style lang="scss" scoped>
 @use '@/colors.scss';
+
 .coach-reviews {
   padding:1.5em;
-  .coach {
-    display:flex;
-    align-items: center;
-    gap:1em;
-    padding: 0 0 1em 0;
-    border-bottom: 2px solid colors.$background-4;
-    .name {
-      font-size: large;
-      font-weight: 500;
-    }
-  }
 }
 </style>
