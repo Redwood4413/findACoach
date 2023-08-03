a6aeafe59f57a10d2dfc0ae44b75a9ed85914f19
Redwood <redwoodredwoodowski@gmail.com>
2023-08-04 01:09:08 +0200

    feat: init

---
+++ src/components/coaches/CoachRatePoint.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+
+export default {
+  name: 'CoachRatePoint',
+  props: {
+    rate: {
+      type: Number,
+      required: true,
+    },
+    index: {
+      type: Number,
+      required: true,
+    },
+  },
+  methods: {
+    isFilled(index: number): boolean {
+      if (index - 1 < this.rate) {
+        return true;
+      }
+      return false;
+    },
+    filledClass(index: number): string {
+      return this.isFilled(index) ? 'filled' : '';
+    },
+  },
+
+};
+</script>
+
+<template>
+  <div :class="`point ${filledClass(index)}`" />
+</template>
+
+<style lang="scss" scoped>
+@use '@/colors.scss';
+
+.point {
+  background: colors.$foreground-1;
+  height:13px;
+  width:13px;
+  border-radius: 50%;
+  &.filled {
+    background: colors.$strong-green;
+  }
+}
+
+</style>
