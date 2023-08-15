d34c6ebcc2e3fd640f0ad4ae3dd4dd2334848ddc
Redwood <redwoodredwoodowski@gmail.com>
2023-08-15 21:28:23 +0200

    feat: add types to Supabase Client

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,7 @@
+import { Database } from '@/types/supabase';
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
+const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_API_KEY,
 );
