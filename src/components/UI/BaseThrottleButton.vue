560ac78f225c0c3260a06e5d9d7c146374fce733
Redwood <redwoodredwoodowski@gmail.com>
2023-08-15 20:59:35 +0200

    feat: init

---
+++ src/components/UI/BaseThrottleButton.vue
@@ -0,0 +1,38 @@
+<script lang="ts">
+export default {
+  name: 'BaseThrottleButton',
+  emits: ['click'],
+  data: () => ({
+    isAbleToReload: true,
+    timeout: 2000,
+  }),
+  methods: {
+    throttleCall() {
+      if (!this.isAbleToReload) return;
+      this.$emit('click');
+      this.isAbleToReload = false;
+      setTimeout(() => {
+        this.isAbleToReload = true;
+      }, this.timeout);
+    },
+  },
+};
+</script>
+
+<template>
+  <BaseButton
+    type="button"
+    :disabled="!isAbleToReload"
+    @click="throttleCall"
+  >
+    <slot />
+  </BaseButton>
+</template>
+
+<style lang="scss" scoped>
+
+  button:disabled {
+    cursor: not-allowed;
+    opacity: .5;
+  }
+</style>
