5250ef366e611e16764ed53f1fd9ed17c4f4bde5
Redwood <redwoodredwoodowski@gmail.com>
2023-08-15 20:54:41 +0200

    feat: init

---
+++ src/components/NoPermissions.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+export default {
+  name: 'NoPermission',
+};
+</script>
+
+<template>
+  <BaseWrapper>
+    <div class="no-permission">
+      <h2>You have no permissions to view this page.</h2>
+      <BaseRouterLink mode="rounded" :to="{ name: 'home' }">Home</BaseRouterLink>
+    </div>
+  </BaseWrapper>
+</template>
+
+<style lang="scss" scoped>
+  .no-permission {
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    min-height:300px;
+    height:100%;
+    width:100%;
+  }
+</style>
