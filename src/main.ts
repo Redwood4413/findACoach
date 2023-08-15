ec9713062d11e7b20999aca89067c8fc4ff0d701
Redwood <redwoodredwoodowski@gmail.com>
2023-08-15 20:54:20 +0200

    feat: add BaseThrottleButton

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,17 @@ import BaseButton from './components/UI/BaseButton.vue';
 import BaseDialog from './components/UI/BaseDialog.vue';
 import BaseWrapper from './components/UI/BaseWrapper.vue';
 import BaseRouterLink from './components/UI/BaseRouterLink.vue';
+import BaseThrottleButton from './components/UI/BaseThrottleButton.vue';
 
 const app = createApp(App);
 
 app.use(store);
 app.use(router);
+
 app.component('BaseButton', BaseButton);
 app.component('BaseDialog', BaseDialog);
 app.component('BaseWrapper', BaseWrapper);
 app.component('BaseRouterLink', BaseRouterLink);
+app.component('BaseThrottleButton', BaseThrottleButton);
 
 app.mount('#app');
