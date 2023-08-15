885b63f7d8659e751753e59f7505bc54ac4be9f1
Redwood <redwoodredwoodowski@gmail.com>
2023-08-15 20:59:48 +0200

    feat: init

---
+++ src/components/UI/BaseError.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="base-error">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 17q.425 0 .713-.288T13 16q0-.425-.288-.713T12 15q-.425 0-.713.288T11 16q0 .425.288.713T12 17Zm0 5q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Zm0-9q.425 0 .713-.288T13 12V8q0-.425-.288-.713T12 7q-.425 0-.713.288T11 8v4q0 .425.288.713T12 13Z" /></svg>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'BaseError',
+};
+</script>
+
+<style lang="scss" scoped>
+@use '@/colors.scss';
+  .base-error {
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    svg {
+      width:100px;
+      fill:colors.$red;
+    }
+  }
+</style>
---
+++ src/components/UI/BaseSuccess.vue
@@ -0,0 +1,79 @@
+<script lang="ts">
+// CREDITS: https://codepen.io/kuvinod5/pen/WNvzazr
+
+export default {
+  name: 'BaseSuccess',
+
+};
+</script>
+
+<template>
+  <div class="success-animation">
+    <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none" /><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" /></svg>
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/colors.scss';
+
+$green: rgb(49, 139, 52);
+$soft-green:rgb(70, 209, 75);
+.success-animation {
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  height:100%;
+  flex-direction: column;
+}
+
+.checkmark {
+  width: 100px;
+  border-radius: 50%;
+  stroke-width: 2;
+  stroke: $green;
+  stroke-miterlimit: 10;
+  box-shadow: inset 0px 0px 0px $green;
+  animation: fill .4s ease-in-out .4s forwards, scale .3s ease-in-out .9s both;
+  position:relative;
+}
+.checkmark__circle {
+    stroke-dasharray: 166;
+    stroke-dashoffset: 166;
+    stroke-width: 2;
+    stroke:$green;
+    stroke-miterlimit: 10;
+    fill: $soft-green;
+    animation: stroke 0.6s cubic-bezier(0.65, 0, 0.45, 1) forwards;
+
+}
+
+.checkmark__check {
+    transform-origin: 50% 50%;
+    stroke-dasharray: 48;
+    stroke-dashoffset: 48;
+    animation: stroke 0.3s cubic-bezier(0.65, 0, 0.45, 1) 0.8s forwards;
+}
+
+@keyframes stroke {
+    100% {
+        stroke-dashoffset: 0;
+    }
+}
+
+@keyframes scale {
+    0%, 100% {
+        transform: none;
+    }
+
+    50% {
+        transform: scale3d(1.1, 1.1, 1);
+    }
+}
+
+@keyframes fill {
+    100% {
+        box-shadow: inset 0px 0px 0px 30px $green;
+    }
+}
+</style>
