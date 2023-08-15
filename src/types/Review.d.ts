6e265cf9940a61a57b0b5aa0cb00bb68cbd9eda5
Redwood <redwoodredwoodowski@gmail.com>
2023-08-15 21:31:21 +0200

    feat: change types to be optional

--- src/types/Review.d.ts
+++ src/types/Review.d.ts
@@ -1,7 +1,10 @@
 interface Review {
-  coachId: string,
+  reviewId: string,
+  userId: string,
   authorId: string,
-  addedAt: number,
+  firstName?: string,
+  lastName?: string,
   description: string,
   rate: number,
+  createdAt: EpochTimeStamp,
 }
