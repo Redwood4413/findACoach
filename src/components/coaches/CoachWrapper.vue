2c3cd5c6ab47dfea2ba743d497a800a580164f63
Redwood <redwoodredwoodowski@gmail.com>
2023-09-12 20:13:09 +0200

    feat: add navigation guard

--- src/components/coaches/CoachWrapper.vue
+++ src/components/coaches/CoachWrapper.vue
@@ -1,16 +1,11 @@
 <script lang="ts">
 import { RouteRecordName, RouterView } from 'vue-router';
+import { useReviewsStore } from '@/stores/ReviewsStore';
 import BaseDialog from '../UI/BaseDialog.vue';
 
 export default {
   name: 'CoachWrapper',
   components: { BaseDialog, RouterView },
-  mounted() {
-    this.checkIfAbsolutePath(this.$route.name);
-  },
-  beforeRouteUpdate(to) {
-    this.checkIfAbsolutePath(to.name);
-  },
   methods: {
     checkIfAbsolutePath(name: RouteRecordName | null | undefined) {
       if (name === 'details') {
@@ -27,6 +22,18 @@ export default {
     icon() {
       return this.absolutePath ? 'close' : 'return';
     },
+  },
+  mounted() {
+    this.checkIfAbsolutePath(this.$route.name);
+  },
+  beforeRouteUpdate(to) {
+    this.checkIfAbsolutePath(to.name);
+  },
+  beforeRouteEnter: (to, _, next) => {
+    const reviewsStore = useReviewsStore();
+    reviewsStore.fetchReviews(to.params.id as string);
+
+    next();
   },
 };
 </script>
