eaaf6fb1f614c20394ade5558f8767adda6eeca3
Redwood <redwoodredwoodowski@gmail.com>
2023-09-12 20:26:06 +0200

    feat: add several methods

--- src/stores/AuthStore.ts
+++ src/stores/AuthStore.ts
@@ -1,16 +1,75 @@
 import { defineStore } from 'pinia';
 import { computed, ref } from 'vue';
+import supabase from '@/lib/supabaseClient';
+import { useMachine } from '@xstate/vue';
+import { loadingMachine } from '@/machines/loadingMachine';
 
 // eslint-disable-next-line import/prefer-default-export
 export const useAuthStore = defineStore('authStore', () => {
+  const { state: stateMachine, send } = useMachine(loadingMachine);
   const state = ref({
-    isLoggedIn: true,
-    userId: 'teeeeeeeeest',
+    user: {
+      userId: '',
+      firstName: '',
+      lastName: '',
+      age: null,
+      email: '',
+      registerDate: 0,
+      isLoggedIn: false,
+      isCoach: false,
+      isAdmin: false,
+    } as User,
+    isGuest: false,
   });
+  const userId = computed(() => state.value.user.userId);
 
-  const getUserId = computed(() => state.value.userId);
+  const isCoach = computed(() => state.value.user.isCoach);
+
+  const isLoggedIn = computed(() => state.value.user.isLoggedIn);
+
+  const isGuest = computed(() => state.value.isGuest);
+  function setUser(user: User) {
+    state.value.user = user;
+  }
+  const fullName = computed(() =>
+    !isGuest.value
+      ? `${state.value.user.firstName} ${state.value.user.lastName}`
+      : 'Guest',
+  );
+  function handleGuestUser() {
+    send('SUCCESS');
+  }
+  async function fetchUser(id: string = 'heheszki') {
+    send('LOAD');
+    if (isGuest.value) {
+      handleGuestUser();
+      return;
+    }
+    if (state.value.user.isLoggedIn) return;
+    try {
+      const { data: user, error } = await supabase
+        .from('users')
+        .select('*')
+        .eq('userId', id)
+        .single();
+
+      if (error) throw new Error(`Failed!: ${error.message}`);
+
+      setUser(user);
+      send('SUCCESS');
+    } catch (error) {
+      console.log(error);
+      send('ERROR');
+    }
+  }
 
   return {
-    getUserId,
+    userId,
+    setUser,
+    fetchUser,
+    fullName,
+    stateMachine,
+    isLoggedIn,
+    isCoach,
   };
 });
