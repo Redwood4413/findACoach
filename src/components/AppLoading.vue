9665189b8e3159b504d124c92ce1da6f1a9a59aa
Redwood <redwoodredwoodowski@gmail.com>
2023-09-12 18:01:24 +0200

    feat: init

---
+++ src/components/AppLoading.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+export default {
+  name: 'AppLoading',
+};
+</script>
+
+<template>
+  <div class="app-loading">
+    <svg-spinners:bars-scale-middle width="5em" height="5em" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.app-loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+}
+</style>
